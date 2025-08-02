3216f428d67aa469d711b349841792b2b92a13f3
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-08-03 01:12:58 +0200

    fix bags

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,30 +1,14 @@
 'use server';
 
 import ProfilePageComponent from '@/components/profile/ProfileComponent';
-import ProfileComponent from '@/components/profile/ProfileComponent';
-import { cookies } from 'next/headers';
-import { FRONTEND_URL } from '@/config/frontend';
+import { ssrFetchUser } from '@/lib/auth/ssrAuth';
 
 export default async function ProfilePage() {
   try {
-    const cookieStore = await cookies();
-    const accessToken = cookieStore.get('accessToken')?.value;
-
-    if (!accessToken) return <ProfilePageComponent user={null} />;
-
-    const profileRes = await fetch(`${FRONTEND_URL}/api/auth/profile`, {
-      headers: {
-        Cookie: `accessToken=${accessToken}`,
-      },
-      cache: 'no-store',
-    });
-
-    if (!profileRes.ok) return <ProfilePageComponent user={null} />;
-
-    const { user } = await profileRes.json();
+    const { user } = await ssrFetchUser();
     return <ProfilePageComponent user={user} />;
   } catch (err) {
     console.error('Error in profile page:', err);
-    return <ProfileComponent user={null} />;
+    return <ProfilePageComponent user={null} />;
   }
 }
