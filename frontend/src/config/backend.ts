c12deb38f1620cb5242159db1db65f086bd40934
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-09-12 22:44:46 +0200

    fix: finished verification of major bugs

--- frontend/src/config/backend.ts
+++ frontend/src/config/backend.ts
@@ -1,7 +1,4 @@
-// export const BACKEND_URL =
-//   typeof window === 'undefined' ? 'http://nginx' : '/api';
-
 export const BACKEND_URL =
   typeof window === 'undefined'
     ? process.env.INTERNAL_API_URL || 'http://api:3000'
-    : process.env.NEXT_PUBLIC_API_URL || '/api';
+    : process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
